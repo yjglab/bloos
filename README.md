5f3e52884928524a0c8cd3dfb9b49e451dd464f2
yjglab <yjgdesign@gmail.com>
2021-06-22 03:41:13 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# BLOOS
+
+/ => Home
+/join => Join
+/login => Login
+/search => Search
+
+/users/edit => Edit user
+/users/delete => Delete user
+
+/videos/watch => Watch Video
+/videos/edit => Edit Video
+/videos/delete => Delete Video
---
+++ src/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import morgan from "morgan";
+
+const PORT = 4000;
+
+const app = express();
+const logger = morgan("dev");
+
+const home = (req, res) => {
+  return res.send("hello");
+};
+const login = (req, res) => {
+  return res.send("login");
+};
+app.use(logger);
+app.get("/", home);
+app.get("/login", login);
+
+const handleListening = () =>
+  console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
+app.listen(PORT, handleListening);
