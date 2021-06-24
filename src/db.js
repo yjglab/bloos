3e795f9359c407bee9886bc4da37dad1fc29ee7e
yjglab <yjgdesign@gmail.com>
2021-06-25 01:14:54 +0900

    linked mongodb and mongoose

---
+++ src/db.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+mongoose.connect("mongodb://127.0.0.1:27017/bloos", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("🔄 Connected to DB");
+const handleError = (error) => console.log("❌ DB ERROR", error);
+db.on("error", handleError);
+db.once("open", handleOpen);
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,10 +1,10 @@
 import express from "express";
 import {
   watch,
-  upload,
-  deleteVideo,
   getEdit,
   postEdit,
+  getUpload,
+  postUpload,
 } from "../controllers/videoController";
 
 const videoRouter = express.Router();
@@ -12,4 +12,5 @@ const videoRouter = express.Router();
 videoRouter.get("/:id(\\d+)", watch);
 videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
 
+videoRouter.route("/upload").get(getUpload).post(postUpload);
 export default videoRouter;
