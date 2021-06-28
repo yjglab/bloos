fad836a276b36d20fd4cfa46e8706a1ca46e37b3
yjglab <yjgdesign@gmail.com>
2021-06-29 01:42:15 +0900

    session connection, add .env

--- src/db.js
+++ src/db.js
@@ -1,5 +1,7 @@
+require("dotenv").config();
+
 import mongoose from "mongoose";
-mongoose.connect("mongodb://127.0.0.1:27017/bloos", {
+mongoose.connect(process.env.DB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
--- src/middlewares.js
+++ src/middlewares.js
@@ -2,6 +2,6 @@ export const localsMiddleware = (req, res, next) => {
   // pug can access this vars.
   res.locals.loggedIn = Boolean(req.session.loggedIn);
   res.locals.siteName = "BLOOS";
-  console.log(res.locals);
+  res.locals.loggedInUser = req.session.user;
   next();
 };
