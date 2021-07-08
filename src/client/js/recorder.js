2879e35fc342052a8ce00f5411a10f7d92f28618
yjglab <yjgdesign@gmail.com>
2021-07-08 20:51:49 +0900

    added video recording

---
+++ src/client/js/recorder.js
@@ -0,0 +1,39 @@
+const startBtn = document.getElementById("startBtn");
+const recVideo = document.getElementById("preview");
+
+let stream;
+let recorder;
+
+const handleDownload = () => {};
+const handleStop = () => {
+  startBtn.innerText = "DOWNLOAD";
+  startBtn.removeEventListener("click", handleStop);
+  startBtn.addEventListener("click", handleDownload);
+  recorder.stop();
+};
+const handleStart = () => {
+  startBtn.innerText = "STOP";
+  startBtn.removeEventListener("click", handleStart);
+  startBtn.addEventListener("click", handleStop);
+  recorder = new MediaRecorder(stream);
+  recorder.ondataavailable = (e) => {
+    const videoFile = URL.createObjectURL(e.data);
+    recVideo.srcObject = null;
+    recVideo.src = videoFile;
+    recVideo.loop = true;
+    recVideo.play();
+  };
+  recorder.start();
+};
+
+const init = async () => {
+  stream = await navigator.mediaDevices.getUserMedia({
+    audio: false,
+    video: true,
+  });
+  recVideo.srcObject = stream;
+  recVideo.play();
+};
+
+init();
+startBtn.addEventListener("click", handleStart);
